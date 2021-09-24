ab046d7c855adfc7f73ab4a3710d5b7b963e0eea
Getty Orawo <gettyorawo@gmail.com>
2021-09-25 01:42:42 +0300

    release

--- config/config.exs
+++ config/config.exs
@@ -34,6 +34,8 @@ config :fly, :graphql_endpoint, "https://api.fly.io/graphql"
 
 config :fly, :client_api, Fly.ClientLive
 
+config :fly,
+  ecto_repos: [Fly.Repo]
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- lib/fly/repo.ex
+++ lib/fly/repo.ex
@@ -1,5 +1,5 @@
-# defmodule Fly.Repo do
-#   use Ecto.Repo,
-#     otp_app: :fly,
-#     adapter: Ecto.Adapters.Postgres
-# end
+defmodule Fly.Repo do
+  use Ecto.Repo,
+    otp_app: :fly,
+    adapter: Ecto.Adapters.Postgres
+end
